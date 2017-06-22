5fe2b9ffb6b1c3a1d26ff5c968e217511c9d692a
Igor <igorbaesse86@gmail.com>
2017-06-22 10:35:26 -0300

    Criaçaõ da classe de justificativa e proposta de tela

---
+++ PORTALJAGUARAO/PORTALJAGUARAO/Controllers/JustificativadePontoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PORTALJAGUARAO.Controllers
+{
+    public class JustificativadePontoController : Controller
+    {
+        // GET: JustificativadePonto
+        public ActionResult Justificativa()
+        {
+            return View();
+        }
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+    }
+}
---
+++ PORTALJAGUARAO/PORTALJAGUARAO/Models/Justificativa.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PORTALJAGUARAO.Models
+{
+    public class Justificativa
+    {
+        public DateTime DataOcorrencia { get; set; }
+        public String NomeColaborador { get; set; }
+        public List<String> Motivo { get; set; }
+        public String DescJustificativa {get;set;}
+        public Boolean Abonado { get; set; }
+        public String CaminhoArquivo { get; set; }
+
+        public Justificativa()
+        {
+            Motivo = new List<string>();
+            Motivo.Add("Pessoal");
+            Motivo.Add("Problema Técnico no Ponto Eletrônico");
+            Motivo.Add("Atestado Médico");
+            Motivo.Add("Serviços Externos");
+            Motivo.Add("Visita para Captação de Clientes");
+            Motivo.Add("Reunião e Cursos");
+       }
+
+
+
+
+
+        //Buscar do banco a lista de justificativas
+        public void AtualizarListaDeJustificaritavas()
+        {
+
+        }
+
+        public void CadastrarJustificativa(Justificativa NovaJustificativa)
+        {
+
+        }
+
+
+
+    }
+}
