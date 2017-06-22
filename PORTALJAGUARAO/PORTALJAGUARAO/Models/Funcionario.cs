38d1694f1ab6c0ab291dd8401d4dcd0faa45e2a4
Igor <igorbaesse86@gmail.com>
2017-06-22 13:17:06 -0300

    Criação da tela de cadastro

--- PORTALJAGUARAO/PORTALJAGUARAO/Models/Funcionario.cs
+++ PORTALJAGUARAO/PORTALJAGUARAO/Models/Funcionario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Data.SqlClient;
 using System.Linq;
@@ -10,32 +11,48 @@ namespace PORTALJAGUARAO.Models
     public class Funcionario
     {
         [Required(ErrorMessage = "Cpf do cadastro deve ser informado!")]
+        [Display(Name="Cpf")]
         public String Cpf { get; set; }
         public String teste { get; set; }
-
+        [Display(Name = "Data de adminissão")]
         public DateTime DataAdmissao { get; set; }
+        [Display(Name = "Data da demissão")]
         public DateTime DataDemissao  { get; set; }
         public Boolean Atividade { get; set; }
+        [Required(ErrorMessage = "O nome deve ser informado")]
         public String Nome { get; set; }
+        [Display(Name = "Sexo")]
+        [Required(ErrorMessage = "O sexo deve ser informado")]
         public String Sexo { get; set; }
+        [Display(Name = "Data do aniversário")]
         public DateTime DataAnivesario { get; set; }
         public String Idade { get; set; }
+        [Required(ErrorMessage = "O setor deve ser informado")]
         public String Setor { get; set; }
+        [Display(Name = "Estado civil")]
         public String EstadoCivil { get; set; }
+        [Display(Name = "Carteira de indetidade")]
         public String Ci { get; set; }
+        [Display(Name = "Emissão da carteira")]
         public String DataEmissaoci { get; set; }
+        [Display(Name = "Data do Nascimento")]
         public String DataNascimento { get; set; }
+        [Display(Name = "Nome do pai")]
         public String NomePai { get; set; }
+        [Display(Name = "Nome da mãe")]
         public String NomeMae { get; set; }
         public String Cargo { get; set; }
+        [Display(Name = "Numero do pis")]
         public String NumeroPis { get; set; }
         public String Cep { get; set; }
         public String Ctps { get; set; }
         public String Serie { get; set; }
+        [Display(Name = "Titulo eleitoral")]
         public String TituloEleitor { get; set; }
         public String Endereco { get; set; }
         public String Bairro { get; set; }
         public String Cidade { get; set; }
+        [Display(Name = "Uf")]
         public String UF { get; set; }
         public String BancoPagamento { get; set; }
         public String Naturalidade { get; set; }
@@ -44,6 +61,8 @@ namespace PORTALJAGUARAO.Models
         public String Celular { get; set; }
         public String Substituto { get; set; }
         public Conjuge Conjuge { get; set; }
+        [Display(Name = "Observação")]
+        public String observacao { get; set; }
 
 
         public  Boolean CadastrarFuncionario(Funcionario NovoCadastro)
