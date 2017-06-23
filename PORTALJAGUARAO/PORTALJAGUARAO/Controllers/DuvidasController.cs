bf4e427b2a7fedeca5e9cfc27b5af2c0cee866e4
Igor <igorbaesse86@gmail.com>
2017-06-23 09:55:33 -0300

    Criação da tela de hora extra e topo das paginas além de um teste para dúvidas

---
+++ PORTALJAGUARAO/PORTALJAGUARAO/Controllers/DuvidasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PORTALJAGUARAO.Controllers
+{
+    public class DuvidasController : Controller
+    {
+        // GET: Duvidas
+        public ActionResult Duvidas()
+        {
+            return View();
+        }
+    }
+}
--- PORTALJAGUARAO/PORTALJAGUARAO/Models/HoraExtra.cs
+++ PORTALJAGUARAO/PORTALJAGUARAO/Models/HoraExtra.cs
@@ -7,5 +7,13 @@ namespace PORTALJAGUARAO.Models
 {
     public class HoraExtra
     {
+        public String NomeColaborador { get; set; }
+        public DateTime DataHoraExtra { get; set; }
+        public DateTime PrimeiraEntrada { get; set; }
+        public DateTime SegundaEntrada { get; set; }
+        public DateTime SegundaSaida { get; set; }
+        public String TotalDeMinutos { get; set; }
+        public String Observacao { get; set; }
+        public String RegraToleranciaPonto { get; set; }
     }
 }
